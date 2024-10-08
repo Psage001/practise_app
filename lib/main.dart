91ebee0fc64c41a37e1794108365eefdd9f8704e
Psage01 <markus01.erasmus@gmail.com>
2024-10-08 13:36:49 +0200

    Adding Constructors and functions

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,28 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('data'),
-        ),
+        body: GradientConatainer(),
       ),
     ),
   );
+}
+
+
+class GradientConatainer extends StatelessWidget{
+ const GradientConatainer({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          decoration: const BoxDecoration(
+              gradient:
+                  LinearGradient(colors: [Color.fromARGB(221, 147, 6, 6),
+                   Color.fromARGB(255, 226, 10, 10)],
+                   begin: Alignment.topLeft,
+                   end: Alignment.bottomRight,
+                   )),
+          child: const Center(
+            child: Text('timmy', style: TextStyle(fontSize: 20.0, color: Colors.amberAccent, fontWeight: FontWeight.bold  ), ),
+          ),
+        );
+  }
 }
