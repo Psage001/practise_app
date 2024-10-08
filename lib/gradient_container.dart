9d2ede04927816e44f4ea962f8bde34f68597bd4
Psage01 <markus01.erasmus@gmail.com>
2024-10-08 13:47:31 +0200

    Adding of code seperation

---
+++ lib/gradient_container.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class GradientConatainer extends StatelessWidget{
+ const GradientConatainer({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          decoration: const BoxDecoration(
+              gradient:
+                  LinearGradient(colors: [Color.fromARGB(221, 147, 6, 6),
+                   Color.fromARGB(255, 226, 10, 10)],
+                   begin: Alignment.topLeft,
+                   end: Alignment.bottomRight,
+                   )),
+          child: const Center(
+            child: Text('timmy', style: TextStyle(fontSize: 20.0, color: Colors.amberAccent, fontWeight: FontWeight.bold  ), ),
+          ),
+        );
+  }
+}
